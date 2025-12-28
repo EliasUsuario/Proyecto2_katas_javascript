7a03147a7f4417e51ef49ec1a67466840e70a795
EliasUsuario <correoelias84@gmail.com>
2025-12-28 17:07:24 +0100

    ejercicio 14

--- ejercicios-básicos-avanzados/ejercicio-13.js
+++ ejercicios-básicos-avanzados/ejercicio-13.js
@@ -1,4 +1,5 @@
-//13- Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
+//13- Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. 
+// Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
 
 const names = [
   'Peter',
@@ -13,6 +14,22 @@ const names = [
   'Jessica',
   'Marc'
 ];
-function nameFinder(nameList) {
-  // Completar
+function nameFinder(nameList, nameBuscar) {
+  for (let i = 0; i < nameList.length; i++) {
+    if (nameList[i] === nameBuscar) {
+      return "true, posición: " + i;
+    }
+  }
+  return false;
 }
+
+// --- PRUEBA DE LA FUNCIÓN ---
+
+const prueba1 = nameFinder(names, "Tony");
+console.log("prueba de Tony:", prueba1);
+
+const prueba2 = nameFinder(names, "Natasha");
+console.log("prueba de Natasha:", prueba2);
+
+const prueba3 = nameFinder(names, "Magneto");
+console.log("prueba de NoExiste:", prueba3);
---
+++ ejercicios-básicos-avanzados/ejercicio-14.js
@@ -0,0 +1,33 @@
+//14- Contador de repeticiones: Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+// Por ejemplo, que devuelva: code:4, repeat: 1, eat: 1,...
+// Podéis conformar el formato que queráis para la devolución del resultado siempre y cuando sea claro.
+// Puedes usar este array para probar tu función:
+
+const words = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code'
+];
+function repeatCounter(list) {
+  const contador = {};
+  for (let palabra of list) {
+    if (contador[palabra]) {
+      contador[palabra]++;
+    } 
+    else {
+      contador[palabra] = 1;
+    }
+  }
+  return contador;
+}
+
+// PRUEBA DE LA FUNCION
+console.log(repeatCounter(words));

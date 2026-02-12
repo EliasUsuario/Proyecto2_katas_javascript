79c440a7a7a0874a3b0d21161ea9429e93a64424
EliasUsuario <correoelias84@gmail.com>
2026-02-12 11:02:58 +0100

    ejercicio 4 completo

--- ejercicios-es6-asincronia/ejercicio-04.js
+++ ejercicios-es6-asincronia/ejercicio-04.js
@@ -11,9 +11,57 @@ const evenAges = ages2.filter(age => age % 2 === 0);// Filtrado de los números 
 console.log(evenAges); // Resultado: [22, 14, 24, 12, 90]
 
 
+//4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+    	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+        	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+            	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+                ];
 
+const arrayStreamers = streamers.filter(streamer => {
+return streamer.gameMorePlayed === 'League of Legends';
+});
+console.log(arrayStreamers);
 
+/*4.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'u' en su propiedad .name. 
+Recomendamos usar la funcion .includes() para la comprobación.*/
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+    	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+        	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+            	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+                ];
 
+const streamersWithU = streamers2.filter(streamer => {
+return streamer.name.includes('u');
+});
+console.log(streamersWithU);
+
+/*4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan
+el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion
+.includes() para la comprobación.
+Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando
+.age sea mayor que 35.*/
+
+const streamers3 = [
+    	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+        	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+            	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+                	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+                    ];
+
+ const filteredStreamers = streamers.filter(streamer => {
+    const matchesGame = streamer.gameMorePlayed.includes('Legends');
+
+    if (matchesGame && streamer.age > 35) {
+        streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+    }
+                                                    
+return matchesGame;
+});
+
+console.log(filteredStreamers);
 
 
 
